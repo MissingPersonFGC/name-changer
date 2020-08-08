30418754093c79637e8ad8c94e69c6081b26a8fe
Corey Lanier <iam@coreylanier.com>
2020-08-07 22:35:54 -0400

    fixed module items again because instructure fixes nothing but steals code

--- api/_routes/moduleRoute.js
+++ api/_routes/moduleRoute.js
@@ -7,59 +7,69 @@ const { applyMiddleware } = require("../_utils");
 applyMiddleware(middleWare, router);
 
 router.route("/").get(async (req, res) => {
-  const { apiKey: access_token, courseId } = req.query;
-  try {
-    const result = await axios({
-      method: "GET",
-      url: `https://canvas.instructure.com/api/v1/courses/${courseId}/modules`,
-      headers: {
-        Accept: "application/json+canvas-string-ids",
-      },
-      params: {
-        access_token,
-        per_page: 99999,
-        include: ["items", "content_details"],
-      },
-    });
-    await Promise.all(
-      result.data.map(async (unit) => {
-        if (unit.items_count > 100) {
-          unit.items = [];
-          let currentPage = 1;
-          const totalPages = Math.ceil(unit.items_count / 100);
-          async function apiPagination() {
-            console.log(currentPage);
-            const itemRes = await axios({
-              method: "GET",
-              url: `https://canvas.instructure.com/api/v1/courses/${courseId}/modules/${unit.id}/items`,
-              headers: {
-                Accept: "application/json+canvas-string-ids",
-              },
-              params: {
-                access_token,
-                per_page: 100,
-                page: currentPage,
-              },
-            });
-            itemRes.data.forEach((item) => {
-              unit.items.push(item);
-            });
-            if (currentPage < totalPages) {
-              currentPage = currentPage + 1;
-              await apiPagination();
-            }
-          }
-          await apiPagination();
-        }
-      })
-    );
+	const { apiKey: access_token, courseId } = req.query;
+	try {
+		const result = await axios({
+			method: "GET",
+			url: `https://canvas.instructure.com/api/v1/courses/${courseId}/modules`,
+			headers: {
+				Accept: "application/json+canvas-string-ids",
+			},
+			params: {
+				access_token,
+				per_page: 99999,
+				include: ["items", "content_details"],
+			},
+		});
+		await Promise.all(
+			result.data.map(async (unit) => {
+				if (unit.items_count > 100) {
+					unit.items = [];
+					let currentPage = 1;
+					const totalPages = Math.ceil(unit.items_count / 100);
+					async function apiPagination() {
+						console.log(currentPage);
+						const itemRes = await axios({
+							method: "GET",
+							url: `https://canvas.instructure.com/api/v1/courses/${courseId}/modules/${unit.id}/items`,
+							headers: {
+								Accept: "application/json+canvas-string-ids",
+							},
+							params: {
+								access_token,
+								per_page: 100,
+								page: currentPage,
+							},
+						});
+						itemRes.data.forEach((item) => {
+							if (item.published) {
+								unit.items.push(item);
+							}
+						});
+						if (currentPage < totalPages) {
+							currentPage = currentPage + 1;
+							await apiPagination();
+						}
+					}
+					await apiPagination();
+				} else {
+					const publishedItems = [];
+					unit.items.forEach((item) => {
+						if (item.published) {
+							publishedItems.push(item);
+						}
+					});
+					unit.items = publishedItems;
+				}
+			})
+		);
 
-    res.status(200).json({
-      data: result.data,
-    });
-  } catch (e) {
-    res.status(400);
-  }
+		res.status(200).json({
+			data: result.data,
+		});
+	} catch (e) {
+		res.status(400);
+	}
 });
 
 exports.router = router;
